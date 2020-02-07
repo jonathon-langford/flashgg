267e888f1c04a99f5ccab86f684c358a1e316f50
Edward Scott <e.scott15@ic.ac.uk>
2020-02-07 05:55:12 +0100

    update run files

--- Systematics/test/runSTXS/run_sig_2016.sh
+++ Systematics/test/runSTXS/run_sig_2016.sh
@@ -1,2 +1,1 @@
- 
-fggRunJobs.py --load sig_2016.json -d Sig2016_21Nov19 --stage-to /eos/home-e/escott/HggLegacy/WorkspaceTest/Pass1/2016/Sig/Raw/ -x cmsRun workspaceStd.py maxEvents=-1 -q workday -n 50 --no-copy-proxy  dumpWorkspace=True doStageOne=True doSystematics=True useParentDataset=True
+fggRunJobs.py --load sig_2016.json -d Sig2016_7Feb20 --stage-to /eos/home-e/escott/HggLegacy/WorkspaceTest/Pass2/2016/Sig/Raw/ -x cmsRun workspaceStd.py maxEvents=-1 -q workday -n 50 --no-copy-proxy  dumpWorkspace=True doStageOne=True doSystematics=True useParentDataset=True
